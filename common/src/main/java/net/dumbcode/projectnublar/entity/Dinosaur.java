dda25035406451e05f4b853a5f50522468256d81
Tfarcenim <44327798+Tfarcenim@users.noreply.github.com>
2025-04-02 06:36:35 -0400

    use the correct sbl and start AI

--- common/src/main/java/net/dumbcode/projectnublar/entity/Dinosaur.java
+++ common/src/main/java/net/dumbcode/projectnublar/entity/Dinosaur.java
@@ -6,17 +6,34 @@ import net.dumbcode.projectnublar.entity.api.FossilRevived;
 import net.dumbcode.projectnublar.init.DataSerializerInit;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.syncher.EntityDataAccessor;
-import net.minecraft.network.syncher.EntityDataSerializer;
-import net.minecraft.network.syncher.EntityDataSerializers;
 import net.minecraft.network.syncher.SynchedEntityData;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.Mth;
-import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.PathfinderMob;
-import net.minecraft.world.entity.ai.goal.WaterAvoidingRandomStrollGoal;
-import net.minecraft.world.item.DyeColor;
+import net.minecraft.world.entity.ai.Brain;
+import net.minecraft.world.entity.ai.behavior.BehaviorUtils;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
+import net.tslat.smartbrainlib.api.SmartBrainOwner;
+import net.tslat.smartbrainlib.api.core.BrainActivityGroup;
+import net.tslat.smartbrainlib.api.core.SmartBrainProvider;
+import net.tslat.smartbrainlib.api.core.behaviour.FirstApplicableBehaviour;
+import net.tslat.smartbrainlib.api.core.behaviour.OneRandomBehaviour;
+import net.tslat.smartbrainlib.api.core.behaviour.custom.attack.AnimatableMeleeAttack;
+import net.tslat.smartbrainlib.api.core.behaviour.custom.look.LookAtTarget;
+import net.tslat.smartbrainlib.api.core.behaviour.custom.misc.Idle;
+import net.tslat.smartbrainlib.api.core.behaviour.custom.move.MoveToWalkTarget;
+import net.tslat.smartbrainlib.api.core.behaviour.custom.path.SetRandomWalkTarget;
+import net.tslat.smartbrainlib.api.core.behaviour.custom.path.SetWalkTargetToAttackTarget;
+import net.tslat.smartbrainlib.api.core.behaviour.custom.target.InvalidateAttackTarget;
+import net.tslat.smartbrainlib.api.core.behaviour.custom.target.SetPlayerLookTarget;
+import net.tslat.smartbrainlib.api.core.behaviour.custom.target.SetRandomLookTarget;
+import net.tslat.smartbrainlib.api.core.behaviour.custom.target.TargetOrRetaliate;
+import net.tslat.smartbrainlib.api.core.sensor.ExtendedSensor;
+import net.tslat.smartbrainlib.api.core.sensor.vanilla.HurtBySensor;
+import net.tslat.smartbrainlib.api.core.sensor.vanilla.NearbyLivingEntitySensor;
 import software.bernie.geckolib.animatable.GeoEntity;
 import software.bernie.geckolib.core.animatable.GeoAnimatable;
 import software.bernie.geckolib.core.animatable.instance.AnimatableInstanceCache;
@@ -31,18 +48,22 @@ import software.bernie.geckolib.util.GeckoLibUtil;
 import java.util.List;
 import java.util.Objects;
 
-public class Dinosaur extends PathfinderMob implements FossilRevived, GeoEntity {
+public class Dinosaur extends PathfinderMob implements FossilRevived, GeoEntity, SmartBrainOwner<Dinosaur> {
     public static EntityDataAccessor<DinoData> DINO_DATA = SynchedEntityData.defineId(Dinosaur.class, DataSerializerInit.DINO_DATA);
     public final AnimatableInstanceCache cache = GeckoLibUtil.createInstanceCache(this);
-    private List<String> idleAnimations = List.of("sniffingair", "sniffground", "speak1", "lookleft", "lookright", "scratching","shakehead","shakebody");
+    private List<String> idleAnimations = List.of("sniffingair", "sniffground", "speak1", "lookleft", "lookright", "scratching", "shakehead", "shakebody");
+
     public Dinosaur(EntityType<? extends PathfinderMob> $$0, Level $$1) {
         super($$0, $$1);
     }
 
+    static final String MAIN_CONTROLLER = "controller";
+
     @Override
     public void registerControllers(AnimatableManager.ControllerRegistrar controllers) {
-        controllers.add(new AnimationController<>(this, "controller", 20, this::predicate));
+        controllers.add(new AnimationController<>(this, MAIN_CONTROLLER, 20, this::predicate));
     }
+
     public DinoData getDinoData() {
         return this.entityData.get(DINO_DATA);
     }
@@ -53,10 +74,10 @@ public class Dinosaur extends PathfinderMob implements FossilRevived, GeoEntity 
     }
 
     public Color layerColor(int layer, DinoLayer dinoLayer) {
-        if(dinoLayer != null && dinoLayer.getBasicLayer() == -1){
+        if (dinoLayer != null && dinoLayer.getBasicLayer() == -1) {
             return new Color(0xFFFFFFFF);
         }
-        if(layer >= this.getDinoData().getLayerColors().stream().count()){
+        if (layer >= this.getDinoData().getLayerColors().stream().count()) {
             return new Color(Mth.floor(this.getDinoData().getLayerColor(dinoLayer.getBasicLayer())));
         }
         return new Color(Mth.floor(this.getDinoData().getLayerColor(layer)));
@@ -81,26 +102,22 @@ public class Dinosaur extends PathfinderMob implements FossilRevived, GeoEntity 
         entityData.set(DINO_DATA, DinoData.fromNBT($$0.getCompound("dino_data")));
     }
 
-    @Override
-    protected void registerGoals() {
-        this.goalSelector.addGoal(0, new WaterAvoidingRandomStrollGoal(this, 1.0D));
-    }
-
     protected PlayState predicate(AnimationState<GeoAnimatable> state) {
         AnimationController<?> controller = state.getController();
-        if(isMoving()){
+        if (isMoving()) {
             controller.setAnimation(RawAnimation.begin().thenLoop("walk"));
         } else {
-            if(controller.hasAnimationFinished() || (controller.getCurrentAnimation()!=null && Objects.equals(controller.getCurrentAnimation().animation().name(), "walk"))){
+            if (controller.hasAnimationFinished() || (controller.getCurrentAnimation() != null && Objects.equals(controller.getCurrentAnimation().animation().name(), "walk"))) {
                 controller.setAnimation(RawAnimation.begin().thenLoop(idleAnimations.get(this.random.nextInt(idleAnimations.size()))));
             }
         }
         return PlayState.CONTINUE;
     }
 
-    public boolean isMoving(){
+    public boolean isMoving() {
         return this.xo != this.getX() || this.zo != this.getZ();
     }
+
     @Override
     public AnimatableInstanceCache getAnimatableInstanceCache() {
         return cache;
@@ -109,4 +126,65 @@ public class Dinosaur extends PathfinderMob implements FossilRevived, GeoEntity 
     public void setDinoData(DinoData dinoData) {
         this.entityData.set(DINO_DATA, dinoData);
     }
+
+    @Override
+    public List<? extends ExtendedSensor<? extends Dinosaur>> getSensors() {
+        NearbyLivingEntitySensor<Dinosaur> nearbyLivingEntitySensor = new NearbyLivingEntitySensor<>();
+        nearbyLivingEntitySensor.setPredicate((target, entity) -> canTarget(target));
+        return List.of(nearbyLivingEntitySensor, // This tracks nearby entities
+                new HurtBySensor<>());
+    }
+
+    public boolean canTarget(LivingEntity target) {
+        if (target instanceof Dinosaur) return false;
+        if (target instanceof Player player && player.isCreative()) return false;
+        if (target.isDeadOrDying()) return false;
+        if (target.getVehicle() == this) return false;
+        return true;
+    }
+
+    @Override
+    public BrainActivityGroup<? extends Dinosaur> getCoreTasks() {
+        return BrainActivityGroup.coreTasks(
+                new LookAtTarget<>(),                      // Have the entity turn to face and look at its current look target
+                new MoveToWalkTarget<>());
+    }
+
+    @Override
+    protected Brain.Provider<Dinosaur> brainProvider() {
+        return new SmartBrainProvider<>(this);
+    }
+
+    @Override
+    protected void customServerAiStep() {
+        super.customServerAiStep();
+        tickBrain(this);
+    }
+
+    @Override
+    public BrainActivityGroup<? extends Dinosaur> getIdleTasks() {
+        // These are the tasks that run when the mob isn't doing anything else (usually)
+        return BrainActivityGroup.idleTasks(
+                new FirstApplicableBehaviour<>(      // Run only one of the below behaviours, trying each one in order.
+                        new TargetOrRetaliate<>()
+                                .attackablePredicate(entity -> canTarget(entity)),            // Set the attack target and walk target based on nearby entities
+                        // Set the attack target and walk target based on nearby entities
+                        new SetPlayerLookTarget<>(),          // Set the look target for the nearest player
+                        new SetRandomLookTarget<>()),         // Set a random look target
+                new OneRandomBehaviour<>(                 // Run a random task from the below options
+                        new SetRandomWalkTarget<>().setRadius(7, 7),          // Set a random walk target to a nearby position
+                        new Idle<>().runFor(entity -> entity.getRandom().nextInt(30, 60)))); // Do nothing for 1.5->3 seconds
+    }
+
+    @Override
+    public BrainActivityGroup<? extends Dinosaur> getFightTasks() {
+        return BrainActivityGroup.fightTasks(
+                new InvalidateAttackTarget<>()
+                        .invalidateIf((entity, target) -> !isInWaterOrBubble() || target instanceof Player pl && (pl.isCreative() || pl.isSpectator())), // Cancel fighting if the target is no longer valid
+                new SetWalkTargetToAttackTarget<>().speedMod((owner, target) -> 1.5f),      // Set the walk target to the attack target
+                
+                new AnimatableMeleeAttack<>(4)
+        ); // Melee attack the target if close enough
+    }
+
 }
